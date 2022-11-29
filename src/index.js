6c538fdf10da45554fe8a16ad19ed4f11e3714ba
jash-debug <jash-debug@github.com>
2022-11-29 22:25:11 +0300

    set up linter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
