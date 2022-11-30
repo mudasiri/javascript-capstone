51cd2ba4a43d03f5122aff0810bdbee1b05e740e
jash-debug <jash-debug@github.com>
2022-11-30 13:51:39 +0300

    add javascript functionality to load popup dynamically

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import './style.css';
 import { getAllAstronauts } from './modules/helper.js';
 
 const closeDemo = document.querySelector('.close');
-closeDemo.addEventListener ('click', () =>{
+closeDemo.addEventListener('click', () => {
   document.getElementById('overlay-project').style.display = 'none';
 });
-
 
 window.addEventListener('DOMContentLoaded', () => {
   getAllAstronauts();
 });
-
--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,21 +1,24 @@
 // grab main tag displaying astronauts items
 const astronautList = document.getElementById('astronaut');
 
-const showPopup = (astronaut) => {
-  document.getElementById('overlay-project').style.display = 'block';
-}
-
 // create arrow function to loop through and display austraunauts lists from API
 export const displayAstronauts = (astronauts) => {
   astronautList.innerHTML = '';
   astronauts.forEach((astronaut) => {
     // append div to main tag
     astronautList.insertAdjacentHTML('beforeend', `<div class='person'><img class='person-img' src= '${astronaut.image}' alt='${astronaut.name}-image'> <div class='title-area'><h2>${astronaut.name}</h2> <i class="heart fa-regular fa-heart"></i> </div> <button class="comment-${astronaut.id}">Comments</button><button>Reservations</button></div>`);
+
+    document.querySelector(`.comment-${astronaut.id}`).addEventListener('click', () => {
+      document.getElementById('overlay-project').style.display = 'block';
+      document.querySelector('.popup-image').src = `${astronaut.image}`;
+      document.querySelector('.popup-title').innerHTML = `${astronaut.name}`;
+      document.querySelector('.country').innerHTML = `Country: ${astronaut.country}`;
+      document.querySelector('.agency').innerHTML = `Agency: ${astronaut.agency}`;
+      document.querySelector('.position').innerHTML = `Position: ${astronaut.position}`;
+      document.querySelector('.spacecraft').innerHTML = `Spacecraft: ${astronaut.spacecraft}`;
+    });
   });
 };
-document.querySelector('.comment-`${astronaut.id}`').addEventListener('click', () => {
-  document.getElementById('overlay-project').style.display = 'block';
-});
 
 // get all Austronauts currently in Space from API
 export const getAllAstronauts = async () => {
@@ -34,4 +37,3 @@ export const getAllAstronauts = async () => {
     return error;
   }
 };
-
